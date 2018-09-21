0f5993a9b1264c0af69796fe9181a6b7192b1ca8
Anderson Cardoso <ancardos@Andersons-MacBook-Pro.local>
2018-09-21 13:52:31 -0400

    initial commit

---
+++ env.sh
@@ -0,0 +1,12 @@
+# Authentication detail
+export TF_VAR_tenancy_ocid=""
+export TF_VAR_user_ocid=""
+export TF_VAR_fingerprint=""
+export TF_VAR_private_key_path=""
+# Region
+export TF_VAR_region=""
+# Comportment
+export TF_VAR_compartment_ocid=""
+# Public/Private key used in instance
+export TF_VAR_private_hey_path=""
+export TF_VAR_public_key_path=""
