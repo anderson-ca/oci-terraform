cc56815d9d2a28526b9d0fc919ff2bfbab9f9cb8
Anderson Cardoso <ancardos@Andersons-MBP.fios-router.home>
2018-10-02 05:27:41 -0400

    add vm file to compute module

--- modules/compute/vm.tf
+++ modules/compute/vm.tf
@@ -2,7 +2,11 @@ variable instance_shape {}
 variable instance_count {}
 variable created_image_id {}
 variable authorized_keys {}
-variable availability_domain { type = "string" }
+
+variable availability_domain {
+  type = "string"
+}
+
 variable available_subnet {}
 
 resource "oci_core_instance" "TFInstance" {
@@ -11,4 +15,25 @@ resource "oci_core_instance" "TFInstance" {
   compartment_id      = "${var.created_compartment_id}"
   display_name        = "test_instance${count.index}"
   shape               = "${var.instance_shape}"
+
+  create_vnic_details {
+    subnet_id        = "${var.available_subnet}"
+    display_name     = "primaryvnic"
+    assign_public_ip = true
+    hostname_label   = "testinstance${count.index}"
+  }
+
+  source_detail {
+    source_type = "image"
+    source_id   = "${var.created_image_id}"
+  }
+
+  metadata {
+    ssh_authorized_keys = "${var.authorized_keys}"
+  }
+
+  timeout {
+    create = "60m"
+  }
+
 }
