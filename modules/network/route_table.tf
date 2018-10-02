3083b950414c446128a3ce198b30a7a70c9f13b0
Anderson Cardoso <ancardos@Andersons-MBP.fios-router.home>
2018-10-02 05:44:13 -0400

    add route table to vcn

---
+++ modules/network/route_table.tf
@@ -0,0 +1,24 @@
+resource "oci_core_route_table" "route_table" {
+  compartment_id = "${var.created_compartment_id}"
+  vcn_id         = "${oci_core_virtual_network.vcn.id}"
+  display_name   = "RouteTable"
+
+  route_rules {
+    cidr_block        = "0.0.0.0/0"
+    network_entity_id = "${oci_core_internet_gateway.gateway.id}"
+  }
+}
+
+data "oci_core_route_tables" "created_route_table" {
+  compartment_id = "${var.created_compartment_id}"
+  vcn_id         = "${oci_core_virtual_network.vcn.id}"
+
+  filter {
+    name   = "id"
+    values = ["${oci_core_route_table.route_table.id}"]
+  }
+}
+
+output "created_route_tables" {
+  value = "${oci_core_route_table.route_table.id}"
+}
