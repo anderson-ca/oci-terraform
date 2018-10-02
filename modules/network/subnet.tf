4d198fa9e45341ebcaee737d3b66c3beef0d513e
Anderson Cardoso <ancardos@Andersons-MBP.fios-router.home>
2018-10-02 06:37:13 -0400

    add subnets

---
+++ modules/network/subnet.tf
@@ -0,0 +1,32 @@
+variable subnet_count {}
+variable tenancy_ocid {}
+variable availability_domains {type = "list"}
+variable security_list_ids {}
+variable created_route_table {}
+
+resource "oci_ore_subnet" "test-subnet" {
+  count = "${var.subnet_count}"
+  availability_domain = "${lookup(var.availability_domains[count.index], "name")}"
+
+  cidr_block = "10.0.${1 + count.index}.0/24"
+  display_name = "Test Barcus Subnet ${count.index}"
+  dns_label = "ancardos${1 + count.index}"
+  compartment_id = "${var.created_compartment_id}"
+  vcn_id = "${oci_core_virtual_network.vcn.id}"
+  security_list_ids = ["${var.security_list_ids}"]
+  route_table_id = "${var.created_route_table}"
+  dhcp_options_id = "${oci_core_virtual_network.vcn.default_dhcp_options_id}"
+}
+
+data "oci_core_subnets" "test_subnets" {
+  compartment_id = "${var.created_compartment_id}"
+  vcn_id = "${oci_core_virtual_newtwork.vcn.id}"
+}
+
+data "oci_identity_availability_domains" "ADs" {
+  compartment_id = '${var.tenancy_ocid}'
+}
+
+output "created_subnets" {
+  value = "${data.oci_core_subnets.test_subnets.subnets}"
+}
